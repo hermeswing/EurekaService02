413b545002d426a1841851791ac2fe24064f61f7
jypark-ubuntu <hermeswing@naver.com>
2024-03-28 13:34:18 +0900

    Eureka Service #02

---
+++ src/main/java/octopus/EurekaService02Application.java
@@ -0,0 +1,13 @@
+package octopus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaService02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaService02Application.class, args);
+    }
+
+}
---
+++ src/main/java/octopus/service/ServiceController.java
@@ -0,0 +1,53 @@
+package octopus.service;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collections;
+import java.util.Enumeration;
+
+@RestController
+@RequestMapping("/service02")
+@Slf4j
+public class ServiceController {
+    Environment env;
+
+    @Autowired
+    public ServiceController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Service #02 입니다.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Service #02 입니다.";
+    }
+
+    @GetMapping("/check")
+    public String check(HttpServletRequest request) {
+        Enumeration<String> headers = request.getHeaderNames();
+        Collections.list(headers).stream().forEach(name -> {
+            Enumeration<String> values = request.getHeaders(name);
+            Collections.list(values).stream().forEach(value -> System.out.println(name + "=" + value));
+        });
+
+        log.info("Server port={}", request.getServerPort());
+
+        log.info("spring.cloud.client.hostname={}", env.getProperty("spring.cloud.client.hostname"));
+        log.info("spring.cloud.client.ip-address={}", env.getProperty("spring.cloud.client.ip-address"));
+
+        return String.format("Service #01 입니다. PORT %s"
+                , env.getProperty("local.server.port"));
+    }
+}
